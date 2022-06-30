39c5cbee9ddd5791ff46a2cd25361d225817ea5d
Humza Afzal <humza.afzal77@gmail.com>
2022-06-30 20:09:46 +0000

    finished java transactions

--- core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/DatabaseConnectionManager.java
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/DatabaseConnectionManager.java
@@ -6,13 +6,14 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class DatabaseConnectionManager {
+
   private final String url;
   private final Properties properties;
 
   public DatabaseConnectionManager(String host, String databaseName,
-                                   String username, String password) {
+      String username, String password) {
 
-    this.url = "jdbc:postgresql://"+host+"/"+databaseName;
+    this.url = "jdbc:postgresql://" + host + "/" + databaseName;
     this.properties = new Properties();
     this.properties.setProperty("user", username);
     this.properties.setProperty("password", password);
--- core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
@@ -2,36 +2,41 @@ package ca.jrvs.apps.jdbc;
 
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.sql.ResultSet;
 
 public class JDBCExecutor {
 
   public static void main(String[] args) {
-    DatabaseConnectionManager dcm = new DatabaseConnectionManager("localhost", "hplussport", "postgres", "password");
+    DatabaseConnectionManager dcm = new DatabaseConnectionManager("localhost", "hplussport",
+        "postgres", "password");
     try {
       Connection connection = dcm.getConnection();
       CustomerDAO customerDAO = new CustomerDAO(connection);
-      Customer customer = new Customer();
-      customer.setFirstName("John");
-      customer.setLastName("Adams");
-      customer.setEmail("jadams@wh.gov");
-      customer.setAddress("1234 Main St");
-      customer.setCity("Arlington");
-      customer.setState("VA");
-      customer.setPhone("(555) 555-9845");
-      customer.setZipCode("01234");
-
-      Customer dbCustomer = customerDAO.create(customer);
-      System.out.println(dbCustomer);
-      dbCustomer = customerDAO.findById(dbCustomer.getId());
-      System.out.println(dbCustomer);
-      dbCustomer.setEmail("john.adams@wh.gov");
-      dbCustomer = customerDAO.update(dbCustomer);
-      System.out.println(dbCustomer);
-      customerDAO.delete(dbCustomer.getId());
-    }
-    catch(SQLException e) {
+      customerDAO.findAllSorted(20).forEach(System.out::println);
+      System.out.println("PAGED");
+      for (int i = 1; i < 3; i++) {
+        System.out.println("Page number: " + i);
+        customerDAO.findAllPaged(10, i).forEach(System.out::println);
+      }
+//      CustomerDAO customerDAO = new CustomerDAO(connection);
+//      Customer customer = new Customer();
+//      customer.setFirstName("John");
+//      customer.setLastName("Adams");
+//      customer.setEmail("jadams@wh.gov");
+//      customer.setAddress("1234 Main St");
+//      customer.setCity("Arlington");
+//      customer.setState("VA");
+//      customer.setPhone("(555) 555-9845");
+//      customer.setZipCode("01234");
+//
+//      Customer dbCustomer = customerDAO.create(customer);
+//      System.out.println(dbCustomer);
+//      dbCustomer = customerDAO.findById(dbCustomer.getId());
+//      System.out.println(dbCustomer);
+//      dbCustomer.setEmail("john.adams@wh.gov");
+//      dbCustomer = customerDAO.update(dbCustomer);
+//      System.out.println(dbCustomer);
+//      customerDAO.delete(dbCustomer.getId());
+    } catch (SQLException e) {
       e.printStackTrace();
     }
   }
