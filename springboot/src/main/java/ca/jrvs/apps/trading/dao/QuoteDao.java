abe1d3647926462d1802be3e40488148bf8b7f2c
Humza Afzal <humza.afzal77@gmail.com>
2022-08-10 20:38:53 +0000

    Stuck on springboot

---
+++ springboot/src/main/java/ca/jrvs/apps/trading/dao/QuoteDao.java
@@ -0,0 +1,14 @@
+package ca.jrvs.apps.trading.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class QuoteDao {
+
+  @Autowired
+  public QuoteDao() {
+
+  }
+
+}
