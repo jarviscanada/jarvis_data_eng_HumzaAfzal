0bee2aded5143d1c75224e1a859ee2551bd8db50
Humza Afzal <humza.afzal77@gmail.com>
2022-08-22 20:07:05 +0000

    implemented traderDao

---
+++ springboot/src/main/java/ca/jrvs/apps/trading/model/domain/Trader.java
@@ -0,0 +1,14 @@
+package ca.jrvs.apps.trading.model.domain;
+
+public class Trader implements Entity<Integer> {
+
+  @Override
+  public Integer getId() {
+    return null;
+  }
+
+  @Override
+  public void setId(Integer integer) {
+
+  }
+}
---
+++ springboot/src/test/java/ca/jrvs/apps/trading/dao/TraderDaoIntTest.java
@@ -0,0 +1,30 @@
+package ca.jrvs.apps.trading.dao;
+
+import org.junit.Test;
+
+public class TraderDaoIntTest {
+
+  @Test
+  public void getJdbcTemplate() {
+  }
+
+  @Test
+  public void getSimpleJdbcInsert() {
+  }
+
+  @Test
+  public void getTableName() {
+  }
+
+  @Test
+  public void getIdColumnName() {
+  }
+
+  @Test
+  public void getEntityClass() {
+  }
+
+  @Test
+  public void updateOne() {
+  }
+}
